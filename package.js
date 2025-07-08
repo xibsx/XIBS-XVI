f7e2df8bc76eeebec38b7fc83e8a32952451960c
xibsx <xibstech@gmail.com>
2025-07-08 06:08:29 +0300

    Create package.js

---
+++ package.js
@@ -0,0 +1,13 @@
+{
+  "name": "whatsapp-bot",
+  "version": "1.0.0",
+  "description": "A basic WhatsApp bot using whatsapp-web.js",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "qrcode-terminal": "^0.12.0",
+    "whatsapp-web.js": "^1.21.0"
+  }
+}
